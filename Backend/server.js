4a252ee688134b406df334722a0995843626eff8
Mayank <mastermanku2006@gmail.com>
2026-03-09 00:52:02 +0530

    improved error handling

--- Backend/server.js
+++ Backend/server.js
@@ -3,14 +3,11 @@ const cors = require('cors')
 const app = express()
 require('dotenv').config()
 const mongoose = require('mongoose')
-
 const Secret = require ('./schema.js')
 
 
 const port = process.env.PORT
 const mongoUri = process.env.MONGO_URI
-
-
 
 mongoose.connect(mongoUri)
     .then(
@@ -27,24 +24,25 @@ app.use(cors())
 app.post('/create', async(req,res)=>{
     const {msg} = req.body
     const secret = await Secret.create({msg})
+    console.log("\nNew secret created:")
     console.log (secret)
     const link = `localhost:5173/secret/${secret._id}`
-    res.status(201).json({msg : "Secret message received", secret: secret, link : link})
+    res.status(201).json({msg : "Secret message received",secret: secret, link : link})
 })
 
 app.get('/secret/:id', async(req,res)=>{
     try {
         const secret = await Secret.findByIdAndDelete(req.params.id)
         if (!secret) {
-            return res.status(404).json({msg: 'X'})
+            return res.status(404).json({msg: "Secret Exploded", err:true, desc:"This secret has already been viewed and permanently deleted from our servers."})
         }
-        res.json({msg: secret.msg})
+        res.json({msg: secret.msg, err:false})
     } catch (error) {
         if (error.name === "CastError") {
-        return res.status(400).json({msg: 'I'}) 
+        return res.status(400).json({msg: "Invalid Link", err:true, desc: "The secret id in this link is not of correct format."}) 
         }
 
-        res.status(500).json({msg: error.name})
+        res.status(500).json({msg:"Server Error", err:true, desc:"Sorry, the server is facing some issues in retrieving your secret message."})
     }
 })
   
